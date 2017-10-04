b411610063b3c46779e89e32b7b4cd27f3f9a1b9
RemyMozelle <timoz974@hotmail.fr>
2017-10-04 15:25:06 +0400

    fix menu

--- src/dentiste.js
+++ src/dentiste.js
@@ -35,7 +35,7 @@ export default class Dentiste extends Component {
 
                 </div>
                <div className="map">
-               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14910.877158345642!2d55.44481059591998!3d-20.883361785267176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x21827fab69a28343%3A0xd87444a35748665!2sDr+Pierre-%C3%89tienne+BULTH%C3%89!5e0!3m2!1sfr!2sus!4v1507100018422"></iframe>
+               <iframe title="dentiste" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14910.877158345642!2d55.44481059591998!3d-20.883361785267176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x21827fab69a28343%3A0xd87444a35748665!2sDr+Pierre-%C3%89tienne+BULTH%C3%89!5e0!3m2!1sfr!2sus!4v1507100018422"></iframe>
                </div>
             </div>
         </div>
