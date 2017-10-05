3fda9829f5892d801c516b2a1c31f2ad6f3b783d
Alexandre Callens <alekzxand@gmail.com>
2017-10-05 08:35:00 +0400

    meuh

---
+++ src/appli.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './index.css'
+export default class Appli extends Component {
+  render() {
+    return (
+      <div>
+        <p>merdd</p>
+      </div>
+    )
+  }
+}
